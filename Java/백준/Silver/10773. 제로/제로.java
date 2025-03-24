d85a1f7f21974fe4e44f38ac3c682a529747ff85
JangMinJu <jmj4701jmj@gmail.com>
2025-03-24 21:07:45 +0900

    [Silver IV] Title: 제로, Time: 208 ms, Memory: 23972 KB -BaekjoonHub

---
+++ Java/백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(bf.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < k; i++) {
+            int temp = Integer.parseInt(bf.readLine());
+            if (temp == 0) stack.pop();
+            else stack.push(temp);
+        }
+
+        int sum = 0;
+        for (int i = 0; i < stack.size(); i++) {
+            sum += stack.get(i);
+        }
+
+        System.out.println(sum);
+    }
+}
